c60d650b4838cc664ca4569e6be30d56a4c26b27
Bunyamin Yahya Saka <65624617+bunyaminsaka@users.noreply.github.com>
2022-12-27 23:44:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+class Program    
+{
+    static void Main()
+    {  
+        bool isPrime = true;
+        int i, j;
+
+        Console.WriteLine("Prime numbers are from 1 --> 10");
+
+        for (i = 2; i <= 10; i++)
+        {
+            for (j = 2; j <= 10; j++)
+            {
+                if (i != j && i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.Write("\t" + i);
+            }
+            isPrime = true;
+        }
+        Console.ReadKey();
+
+    }
+}
